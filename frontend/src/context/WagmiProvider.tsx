641b841501ac7963c47841c132b051d827eb01e0
21Chani <dmendoncacosta03@gmail.com>
2023-01-31 14:42:32 -0300

    testing wallet connect provider

--- frontend/src/context/WagmiProvider.tsx
+++ frontend/src/context/WagmiProvider.tsx
@@ -1,24 +1,24 @@
 import { configureChains, createClient, mainnet, WagmiConfig } from 'wagmi'
+import { alchemyProvider } from '@wagmi/core/providers/alchemy'
 import { publicProvider } from '@wagmi/core/providers/public'
 import { optimismGoerli } from '@wagmi/core/chains'
 import { ConnectKitProvider, getDefaultClient } from 'connectkit'
 import { ReactNode } from 'react'
-const { chains, provider } = configureChains([mainnet, optimismGoerli], [publicProvider()])
-
-const client = createClient(
-  getDefaultClient({
-    appName: 'Exchangett',
-    alchemyId: 'dduxooAO1ELKTf_kXyJHvqIcDniRVvXn',
-    chains,
-  }),
+const { chains, provider } = configureChains(
+  [mainnet, optimismGoerli],
+  [publicProvider(), alchemyProvider({ apiKey: 'dduxooAO1ELKTf_kXyJHvqIcDniRVvXn' })],
 )
 
-export function WagmiProvider({ children }: { children: ReactNode }) {
-  console.log('testing crash')
+const client = createClient({
+  autoConnect: true,
+  provider,
+})
+// getDefaultClient({
+//   appName: 'Exchangett',
+//   alchemyId: 'dduxooAO1ELKTf_kXyJHvqIcDniRVvXn',
+//   chains,
+// }),,
 
-  return (
-    <WagmiConfig client={client}>
-      <ConnectKitProvider>{children}</ConnectKitProvider>
-    </WagmiConfig>
-  )
+export function WagmiProvider({ children }: { children: ReactNode }) {
+  return <WagmiConfig client={client}>{children}</WagmiConfig>
 }
