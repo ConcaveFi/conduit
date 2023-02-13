8bb5dd5594662ec8f3e5f6019f9c13fd1d18d57a
21Chani <dmendoncacosta03@gmail.com>
2023-02-13 14:28:12 -0300

    numeric input

--- frontend/src/components/strategy/OrderFormPanel.tsx
+++ frontend/src/components/strategy/OrderFormPanel.tsx
@@ -1,12 +1,17 @@
-import { ChevronIcon } from '@tradex/icons'
-import { Button, CheckBox, Flex, Panel, Slider, Text } from '@tradex/interface'
-import { useState } from 'react'
+import { ChevronIcon, CloseIcon } from '@tradex/icons'
+import { Button, CheckBox, Flex, Input, Panel, Slider, Text } from '@tradex/interface'
+import { ChangeEvent, useState } from 'react'
 import { CurrencyInput } from '../CurrencyInput'
 import { OrderTab } from './OrderSelector'
 
 export function OrderFormPanel() {
-  const [value, setValue] = useState(0)
-
+  const [value, setValue] = useState<number>()
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    let value = +e.target.value
+    if (value >= 50) value = 50
+    if (value <= 0) value = 0
+    setValue(value || undefined)
+  }
   return (
     <Panel name="Order Form" className="w-3/12 ">
       <OrderTab />
@@ -33,7 +38,27 @@ export function OrderFormPanel() {
         <CheckBox onToggle={() => {}} info="Post" />
         <CheckBox onToggle={() => {}} info="Hidden" />
       </Flex>
-      <Slider pseudoMax={50} track onValue={setValue} />
+      <Flex justify="between" centered>
+        <Text variant="medium">Leverage</Text>
+        <Flex className="w-[40%] justify-end items-center px-2 gap-2 h-12 rounded-full relative bg-ocean-600 ">
+          <Input
+            className="w-[60%] text-end"
+            max={50}
+            min={0}
+            variant="simple"
+            type={'number'}
+            placeholder={'0.0'}
+            value={value || ''}
+            onChange={handleInput}
+          />
+          {!!value && (
+            <Button onClick={() => setValue(0)}>
+              <CloseIcon className="w-3 h-3 fill-ocean-300 absolute left-3" />
+            </Button>
+          )}
+        </Flex>
+      </Flex>
+      <Slider max={50} step={0.1} track onChange={handleInput} value={value || 0} />
       <Flex className="gap-4 -mb-2 mt-4">
         <Button className="w-full" variant="secondary.underline" size="lg">
           Position 0
--- packages/interface/components/primitives/Slider.tsx
+++ packages/interface/components/primitives/Slider.tsx
@@ -5,30 +5,26 @@ import { Flex } from './Flex'
 import { Text } from './Text'
 
 interface RawSliderProps extends PrimitiveInputProps {
-  onValue?: (val: number) => void
   defaultValue?: number
-  pseudoMax?: number
   max?: number
   min?: number
 }
 
 function RawSlider(props: RawSliderProps) {
-  const { max, min, defaultValue = 0, onValue = () => {}, className } = props
-  const diff = props.pseudoMax ? props.pseudoMax / props.max : 1
-  const [value, setValue] = useState(defaultValue)
+  const { max, min, defaultValue, className, ...rest } = props
+  const [value, setValue] = useState<number>(defaultValue)
   const [pressed, setPressed] = useState(false)
   const ref = useRef<HTMLDivElement>()
   const [width, setWidth] = useState(0)
   const size = 14
 
+  useEffect(() => setValue(+props.value), [props.value])
   useEffect(() => setWidth(ref.current?.clientWidth || 0), [ref])
-
   useEffect(() => {
     if (!ref.current) return
     new ResizeObserver((e) => setWidth(e[0].target.clientWidth)).observe(ref.current)
   }, [])
 
-  useEffect(() => onValue(value * diff), [value])
   const left = useMemo(() => {
     let middle = value * (width / max)
     const difference = (middle / width) * 2 - 1
@@ -53,14 +49,14 @@ function RawSlider(props: RawSliderProps) {
       >
         <Flex
           justify="center"
-          className="bg-gradient-to-r  from-[#34EDB340] to-[#00D1FF40] absolute pointer-events-none z-10 rounded-full "
+          className="bg-gradient-to-r from-[#34EDB340] to-[#00D1FF40] absolute pointer-events-none z-10 rounded-full "
           style={{ width: 30, height: 30, left: left - 15 }}
         >
           <Flex
             centered
             className="min-w-[40px] h-10 rounded-lg -mt-12 bg-ocean-600 border-ocean-400 border-2 shadow-xl"
           >
-            <Text>{value * diff}</Text>
+            <Text>{value}</Text>
           </Flex>
         </Flex>
       </Transition>
@@ -71,8 +67,9 @@ function RawSlider(props: RawSliderProps) {
         onInput={(e) => setValue(+e.currentTarget.value)}
         className={`green-slider ${className}`}
         onMouseUp={() => setPressed(false)}
-        value={value}
+        value={value || 0}
         min={min}
+        {...rest}
       />
     </span>
   )
@@ -82,10 +79,8 @@ export interface SliderProps extends RawSliderProps {
 }
 export function Slider({ track = false, ...props }: SliderProps) {
   const { max = 100, min = 0 } = props
-  const pseudoMax = props.pseudoMax || max
-  const step = pseudoMax / 5
   const trackAmount = 5
-  const diff = props.pseudoMax ? props.pseudoMax / max : 1
+  const steps = max / (trackAmount - 1)
 
   if (track)
     return (
@@ -99,11 +94,7 @@ export function Slider({ track = false, ...props }: SliderProps) {
           {new Array(5).fill(0).map((_, i) => (
             <Flex key={i}>
               <Flex className="bg-ocean-300 w-[2px] h-[8px] justify-center" style={{}}>
-                <Text className="absolute mt-2">
-                  {i == 0 && 0}
-                  {i == trackAmount - 1 && pseudoMax}
-                  {i > 0 && i < trackAmount - 1 && step * i + trackAmount * diff * i}
-                </Text>
+                <Text className="absolute mt-2">{i * steps}</Text>
               </Flex>
               {/* {i <= 3 && (
                 <Flex
