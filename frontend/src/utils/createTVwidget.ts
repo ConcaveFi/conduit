86e9e874892444d35ef0e4d55dbd8da0344d2666
21Chani <dmendoncacosta03@gmail.com>
2023-02-14 13:32:21 -0300

    create trading view widget function

---
+++ frontend/src/utils/createTVwidget.ts
@@ -0,0 +1,20 @@
+const DEFAULT_CHART_PROPS = {
+  autosize: true,
+  symbol: 'BINANCE:ETHUSDT',
+  interval: 'D',
+  timezone: 'Etc/UTC',
+  theme: 'dark',
+  style: '1',
+  locale: 'us',
+  toolbar_bg: '#000',
+  enable_publishing: false,
+  allow_symbol_change: true,
+  container_id: 'chart-container',
+} as WidgetProps
+
+function createTradingViewWidget(props?: WidgetProps) {
+  if (!TradingView) return
+  const finalProps = Object.assign(DEFAULT_CHART_PROPS, { ...props })
+  return new TradingView.widget(finalProps)
+}
+export { createTradingViewWidget as createTVwidget }
