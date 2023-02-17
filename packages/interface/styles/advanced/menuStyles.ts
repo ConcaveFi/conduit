6f75dfffdf23a0511ad4340b1ed644a4027c0ec0
21Chani <dmendoncacosta03@gmail.com>
2023-02-17 08:19:55 -0300

    Add menu

---
+++ packages/interface/styles/advanced/menuStyles.ts
@@ -0,0 +1,15 @@
+import { cva } from 'class-variance-authority'
+
+export const menuBodyStyles = cva(cva('absolute mt-2 top-full')(), {
+  variants: {
+    origin: {
+      top: 'origin-top',
+      'top-right': 'origin-top-right',
+      'top-left': 'origin-top-left',
+      center: 'origin-center',
+    },
+  },
+  defaultVariants: {
+    origin: 'top',
+  },
+})
