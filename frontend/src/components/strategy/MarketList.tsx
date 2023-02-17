29e238a3783871f38478cae13dbd7878fd73edbb
21Chani <dmendoncacosta03@gmail.com>
2023-02-17 08:20:04 -0300

    Using menu instead of popover

--- frontend/src/components/strategy/MarketList.tsx
+++ frontend/src/components/strategy/MarketList.tsx
@@ -1,5 +1,5 @@
 import { ChevronIcon } from '@tradex/icons'
-import { Button, ItemInfo, Popover } from '@tradex/interface'
+import { Button, ButtonProps, Card, Flex, ItemInfo, Menu, Popover, Text } from '@tradex/interface'
 import { marketDataAbi } from '@tradex/perps/marketData/marketDataAbi'
 import { parseBytes32String } from 'ethers/lib/utils.js'
 import Image from 'next/image'
@@ -16,46 +16,48 @@ export function MarketList() {
   })
 
   return (
-    <Popover className="h-fit my-auto">
-      <Popover.Button className="gap-6 outline-none">
+    <Menu className={'h-fit my-auto'}>
+      <Menu.Button className="gap-6 outline-none">
         <ItemInfo
           info="BTC Perpetual"
           value="$ 22,910"
           Icon={<Image alt="bitcoin icon" src="/assets/bitcoin.png" width={30} height={30} />}
         />
         <ChevronIcon />
-      </Popover.Button>
-      <Popover.Panel
-        className="w-[360px] h-[500px] left-10 rounded-tl-sm overflow-y-auto p-2 "
-        origin={'top-left'}
-        variant="glass"
+      </Menu.Button>
+      <Menu.Items
         column
+        variant={'glass'}
+        className="w-[360px] h-[500px] left-10 rounded-tl-sm overflow-y-auto p-2"
+        origin={'top-left'}
       >
         {markets?.map(({ asset, price }) => (
-          <MarketButton
-            price={format(price.toBigInt(), 18)}
-            asset={parseBytes32String(asset || '')}
-          />
+          <Menu.Item>
+            {() => (
+              <MarketButton
+                price={format(price.toBigInt(), 18)}
+                asset={parseBytes32String(asset || '')}
+              />
+            )}
+          </Menu.Item>
         ))}
-      </Popover.Panel>
-    </Popover>
+      </Menu.Items>
+    </Menu>
   )
 }
 
-interface MarketButton {
+interface MarketButton extends ButtonProps {
   asset: string
   price: string
   percent?: string
 }
 // css-s6i0el-menu
-function MarketButton({ asset, price, percent }: MarketButton) {
+function MarketButton({ asset, price, percent, ...props }: MarketButton) {
+  const styles = 'gap-6 h-fit justify-between even:bg-ocean-600 p-2 px-3 rounded-lg'
+  const icon = <Image alt="bitcoin icon" src="/assets/bitcoin.png" width={30} height={30} />
   return (
-    <Button className="gap-6 h-fit justify-between even:bg-ocean-600 p-2 px-3 rounded-lg">
-      <ItemInfo
-        info={asset + ' Perpetual '}
-        value={`${asset}-PERP`}
-        Icon={<Image alt="bitcoin icon" src="/assets/bitcoin.png" width={30} height={30} />}
-      />
+    <Button className={styles} {...props}>
+      <ItemInfo info={asset + ' Perpetual '} value={`${asset}-PERP`} Icon={icon} />
       <ItemInfo align="end" info={percent || '0.00%'} value={price} />
     </Button>
   )
