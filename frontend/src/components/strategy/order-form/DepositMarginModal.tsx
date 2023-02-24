0b08496a18475a6342609c0af6886b448fc11b2b
21Chani <dmendoncacosta03@gmail.com>
2023-02-24 17:09:55 -0300

    wire deposit margin modal

--- frontend/src/components/strategy/order-form/DepositMarginModal.tsx
+++ frontend/src/components/strategy/order-form/DepositMarginModal.tsx
@@ -1,8 +1,10 @@
 import { sUSD_ADDRESS } from '@tradex/core'
 import { Button, Modal, ModalProps, Text } from '@tradex/interface'
-import { formatUnits } from 'ethers/lib/utils'
+import { formatUnits, parseEther } from 'ethers/lib/utils'
+import { useMarketTransferMargin, usePrepareMarketTransferMargin } from 'perps-hooks'
 import { useState } from 'react'
 import { CurrencyInput } from 'src/components/CurrencyInput'
+import { useRouteMarket } from 'src/pages/perps'
 import { useNetwork, useToken } from 'wagmi'
 import { optimismGoerli } from 'wagmi/chains'
 
@@ -12,6 +14,11 @@ export function DepositMarginModal(props: ModalProps) {
 
   const address = sUSD_ADDRESS[chain?.id || optimismGoerli.id]
   const { data } = useToken({ address, enabled: Boolean(chain) })
+
+  const market = useRouteMarket()
+  const args = [parseEther(value?.toString() || '0')] as const
+  const { config } = usePrepareMarketTransferMargin({ address: market?.address, args })
+  const { write } = useMarketTransferMargin(config)
 
   return (
     <Modal centered column className="h-fit w-[400px]" space={'medium.eq'} {...props}>
@@ -24,9 +31,15 @@ export function DepositMarginModal(props: ModalProps) {
         value={value}
         currency={data}
       />
-      <Button disabled={!value} className="rounded-lg py-6" variant={'primary.outline'} size="xl">
-        {value && ' Confirm deposit'}
-        {!value && 'Enter an amount'}
+      <Button
+        disabled={!value || !write}
+        onClick={write}
+        className="rounded-lg py-6"
+        variant={'primary.outline'}
+        size="xl"
+      >
+        {Boolean(value) && ' Confirm deposit'}
+        {!Boolean(value) && 'Enter an amount'}
       </Button>
     </Modal>
   )
