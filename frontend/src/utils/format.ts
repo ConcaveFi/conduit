9b08f035a983c7a7ba2ed8d1937c950bb61dfd53
gregs <ukfaeku@gmail.com>
2023-02-25 10:13:09 -0300

    formar usd

--- frontend/src/utils/format.ts
+++ frontend/src/utils/format.ts
@@ -1,30 +1,20 @@
-// import { formatUnits } from 'ethers/lib/utils'
-
 import { FixedNumber } from 'ethers'
-
-// export const format = (
-//   amount: bigint | number,
-//   decimals: bigint | number,
-//   options?: Intl.NumberFormatOptions,
-// ) => {
-//   const f = Intl.NumberFormat('en-US', {
-//     maximumSignificantDigits: 6,
-//     minimumSignificantDigits: 3,
-//     maximumFractionDigits: 2,
-//     minimumFractionDigits: 2,
-//     ...options,
-//   })
-//   return f.format(+formatUnits(amount, decimals))
-// }
 
 export const format = (value: FixedNumber | string, options?: Intl.NumberFormatOptions) => {
   if (typeof value === 'string') value = FixedNumber.from(value || 0)
   const f = Intl.NumberFormat('en-US', {
-    maximumSignificantDigits: 6,
+    maximumSignificantDigits: 3,
     minimumSignificantDigits: 3,
     maximumFractionDigits: 2,
     minimumFractionDigits: 2,
+    // @ts-ignore roundingPriority is experimental
+    roundingPriority: 'morePrecision',
     ...options,
   })
   return f.format(value.toUnsafeFloat())
 }
+
+export const formatUsd = (value: FixedNumber | string) =>
+  format(value, { style: 'currency', currency: 'usd', currencyDisplay: 'narrowSymbol' })
+
+export const formatPercent = (value: FixedNumber | string) => format(value, { style: 'percent' })
