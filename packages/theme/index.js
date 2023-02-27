de7a2dfa20ca9353ccd49b15ad206216aea42e0c
21Chani <dmendoncacosta03@gmail.com>
2023-02-27 18:16:38 -0300

    add ocean variant

--- packages/theme/index.js
+++ packages/theme/index.js
@@ -1,7 +1,9 @@
 const plugin = require('tailwindcss/plugin')
 const slider = require('./plugin/components/slider')
 const tooltip = require('./plugin/components/tooltip')
+const variants = require('./utils/variants')
 
-module.exports = plugin(function definePlugin({ addComponents, theme }) {
+module.exports = plugin(function definePlugin({ addComponents, theme, addVariant }) {
   addComponents([slider(theme), tooltip(theme)])
+  for (let { name, definition } of variants) addVariant(name, definition)
 }, require('./tailwind.config'))
