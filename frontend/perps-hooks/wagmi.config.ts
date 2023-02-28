5b0571d3327c5bf25adbf812992e80629d96d641
gregs <ukfaeku@gmail.com>
2023-02-28 14:12:22 -0300

    update addresses

--- frontend/perps-hooks/wagmi.config.ts
+++ frontend/perps-hooks/wagmi.config.ts
@@ -1,7 +1,7 @@
 import { ContractConfig, defineConfig } from '@wagmi/cli'
 import { erc, react } from '@wagmi/cli/plugins'
 
-import { optimismGoerli } from 'wagmi/chains'
+import { optimism, optimismGoerli } from 'wagmi/chains'
 import { chainLinkFeedAbi, marketAbi, marketDataAbi, marketSettingsAbi } from './abis'
 
 const contracts = [
@@ -9,14 +9,16 @@ const contracts = [
     name: 'MarketData',
     abi: marketDataAbi,
     address: {
-      [optimismGoerli.id]: '0x0D9eFa310a4771c444233B10bfB57e5b991ad529',
+      [optimismGoerli.id]: '0xc880Cd434eD843398a2E20C8C617E6a9C2690A23',
+      [optimism.id]: '0xF7D3D05cCeEEcC9d77864Da3DdE67Ce9a0215A9D',
     },
   },
   {
     name: 'MarketSettings',
     abi: marketSettingsAbi,
     address: {
-      [optimismGoerli.id]: '0x14fA3376E2ffa41708A0636009A35CAE8D8E2bc7',
+      [optimismGoerli.id]: '0xcE6F78bbc9080891b6732D27e5Ca7e866594F927',
+      [optimism.id]: '0x09793Aad1518B8d8CC72FDd356479E3CBa7B4Ad1',
     },
   },
   { name: 'ChainLink', abi: chainLinkFeedAbi },
