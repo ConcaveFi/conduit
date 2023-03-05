ee47215dcd73c3bdd9b138d8cadb3074d233c32e
21Chani <dmendoncacosta03@gmail.com>
2023-03-05 15:07:17 -0300

    remove search input

--- frontend/src/components/topbar/Topbar.tsx
+++ frontend/src/components/topbar/Topbar.tsx
@@ -25,7 +25,9 @@ export function Topbar() {
           {t('futures')}
         </button>
         <button className="btn btn-underline centered w-[120px] rounded-none">Swap</button>
-        <button className="btn btn-underline centered w-[120px]  rounded-none">Leaderboard</button>
+        <button className="btn btn-underline centered w-[120px]  rounded-none">
+          Leaderboard <ChevronIcon className="fill-light-500 ocean:fill-ocean-100 h-3 w-3" />
+        </button>
       </div>
       <div className="flex w-fit items-center gap-6">
         <LocationSelector />
