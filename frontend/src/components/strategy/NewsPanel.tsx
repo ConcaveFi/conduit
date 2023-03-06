6bf6e8f0e0a0affcf2e45d81df5b6ce8ce801a60
21Chani <dmendoncacosta03@gmail.com>
2023-03-06 10:00:52 -0300

    Cleanup news

--- frontend/src/components/strategy/NewsPanel.tsx
+++ frontend/src/components/strategy/NewsPanel.tsx
@@ -1,29 +1,19 @@
+import { useQuery } from '@tanstack/react-query'
 import { Panel, PanelProps } from '@tradex/interface'
 import { useTranslation } from '@tradex/languages'
-import { forwardRef, useEffect, useState } from 'react'
+import { forwardRef } from 'react'
+
+export function useNews() {
+  const query = useQuery(['news'], async function fetchNews() {
+    const response = await (await fetch('/api/news')).json()
+    return response as NewsItem[]
+  })
+  return query
+}
 
 export const NewsPanel = forwardRef<HTMLDivElement, PanelProps>((props, ref) => {
   const { t } = useTranslation()
-  const [news, setNews] = useState<{ title: string; link: string; by: string }[]>([])
-  useEffect(() => {
-    fetchNews()
-  }, [])
-
-  function fetchNews() {
-    const res = fetch('/api/news')
-      .then((data) => data.json() as any)
-      .then((data) => {
-        let sliced = data.slice(0, 3)
-        sliced = sliced.map((d) => {
-          const title = d.title.$
-          const link = d.link
-          const by = d['dc:creator'].$
-          return { title, link, by }
-        })
-        setNews(sliced)
-      })
-  }
-  console.log(news)
+  const { data: news } = useNews()
 
   return (
     <Panel
@@ -38,7 +28,7 @@ export const NewsPanel = forwardRef<HTMLDivElement, PanelProps>((props, ref) => 
         <div className="flex flex-col gap-2 ">
           <span className="text-light-500 ocean:text-ocean-100">{_new.title}</span>
           <span className="text-light-400 ocean:text-ocean-300 text-xs font-medium">
-            {_new.by} • 6 minutes ago
+            {_new.owner} • 6 minutes ago
           </span>
         </div>
       ))}
--- frontend/src/pages/api/news.ts
+++ frontend/src/pages/api/news.ts
@@ -1,19 +1,23 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const response = (await fetch(
-    'https://www.coindesk.com/arc/outboundfeeds/rss/?outputType=json',
-  ).then((data) => data.json())) as {
-    rss: {
-      channel: {
-        item: {
-          title: { $: string }
-          link: string
-          'dc:creater': { $: string }
-        }[]
-      }
-    }
-  }
+const FEED_ENDPOINT = 'https://www.coindesk.com/arc/outboundfeeds/rss/?outputType=json'
 
-  res.status(200).json(response.rss.channel.item)
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const response = (await (await fetch(FEED_ENDPOINT)).json()) as RawCoindeskFeed
+    const items = response.rss.channel.item
+    const mappedItems = items.map(mapRawNewsItem).slice(0, 3)
+    res.status(200).json(mappedItems)
+  } catch (err) {
+    return []
+  }
+}
+
+function mapRawNewsItem(item: RawCoindeskItem): NewsItem {
+  const description = item.description.$
+  const date = item.pubDate
+  const link = item.link
+  const title = item.title.$
+  const owner = item['dc:creator'].$
+  return { title, link, description, date, owner }
 }
