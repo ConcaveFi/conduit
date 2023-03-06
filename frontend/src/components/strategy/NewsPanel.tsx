e80602938a6dc18e051c4ba534bf5f57bf958e55
21Chani <dmendoncacosta03@gmail.com>
2023-03-06 10:26:37 -0300

    ....

--- frontend/src/components/strategy/NewsPanel.tsx
+++ frontend/src/components/strategy/NewsPanel.tsx
@@ -25,17 +25,17 @@ export const NewsPanel = forwardRef<HTMLDivElement, PanelProps>((props, ref) => 
       className="h-[300px] w-full"
       bodyProps={{ className: 'justify-around' }}
     >
-      {news?.map((_new) => {
+      {news?.map((_new, index) => {
         const date = new Date(_new.date)
         const distance = formatDistanceToNowStrict(date)
 
         return (
-          <div className="flex flex-col gap-2 ">
+          <a key={index} className="flex flex-col gap-2 ">
             <span className="text-light-500 ocean:text-ocean-100">{_new.title}</span>
             <span className="text-light-400 ocean:text-ocean-300 text-xs font-medium">
               {_new.owner} • {distance} ago
             </span>
-          </div>
+          </a>
         )
       })}
     </Panel>
