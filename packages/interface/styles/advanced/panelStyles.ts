ec0f8fd50f373bc2570b72effcd7327d979f02ef
gregs <ukfaeku@gmail.com>
2023-03-09 10:20:26 -0300

    panel header border

--- packages/interface/styles/advanced/panelStyles.ts
+++ packages/interface/styles/advanced/panelStyles.ts
@@ -4,7 +4,7 @@ const baseStyles = cva('flex px-3 justify-between items-center w-full min-h-[2.2
 export const panelHeaderStyles = cva(baseStyles(), {
   variants: {
     variant: {
-      primary: ['bg-light-100 border-light-300 ocean:bg-ocean-500 '],
+      primary: ['bg-light-100 border-ocean-400 border-b ocean:bg-ocean-500'],
       secondary: [
         'bg-light-100 border-light-300 ocean:bg-ocean-500 border-b-2 ocean:border-ocean-400',
       ],
@@ -16,7 +16,7 @@ const baseBodyStyles = cva(['flex flex-col flex-grow rounded-b-lg'])
 export const panelBodyStyles = cva(baseBodyStyles(), {
   variants: {
     variant: {
-      primary: [' bg-light-100 ocean:bg-ocean-500 '],
+      primary: ['bg-light-100 ocean:bg-ocean-500'],
       secondary: ['bg-light-100 ocean:bg-ocean-700'],
     },
     spacing: {
