df622688c372e1a743005a285b60a64f884a997a
21Chani <dmendoncacosta03@gmail.com>
2023-03-10 09:06:23 -0300

    Breakpoints

---
+++ frontend/src/utils/contants/breakpoints.ts
@@ -0,0 +1,18 @@
+/**
+ * @description
+ *  BREAKPOINTS it's a variable that match all `@media` variants of tailwind.
+ *
+ * Breakpoint values:
+ * - sm = @media (min-width: 640px)
+ * - md = @media (min-width: 768px)
+ * - lg = @media (min-width: 1024px)
+ * - xl = @media (min-width: 1280px)
+ * - 2xl = @media (min-width: 1536px)
+ * */
+export const BREAKPOINTS = {
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+  '2xl': 1536,
+}
