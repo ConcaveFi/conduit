69fd4f82e78a66ab861884f9df4b3138e1941728
gregs <ukfaeku@gmail.com>
2023-03-14 11:40:20 -0300

    open/close modal

--- frontend/src/components/futures/widgets/AddWidgetOverlay.tsx
+++ frontend/src/components/futures/widgets/AddWidgetOverlay.tsx
@@ -1,39 +1,25 @@
 import { PlusIcon } from '@tradex/icons'
 import { useTranslation } from '@tradex/languages'
-import Link from 'next/link'
-import { useSearchParams } from 'next/navigation'
-import { useRouter } from 'next/navigation'
-import { useCallback, useMemo } from 'react'
-import { UrlModal } from 'src/utils/enum/urlModal'
+import { useState } from 'react'
 import { AddWidgetModal } from './AddWidgetModal'
 
 export function AddWidgetOverlay() {
   const { t } = useTranslation()
-  const router = useRouter()
-  const params = useSearchParams()
 
-  const isOpen = useMemo(() => params?.get('modal') === UrlModal.ADD_WIDGET, [params])
-
-  const closeModal = () => {
-    const newParams = (params || '')
-      .toString()
-      .replaceAll(new RegExp(`modal=(${UrlModal.ADD_WIDGET})`, 'g'), '')
-    router.replace(`?${newParams}`)
-  }
+  const [isOpen, setOpen] = useState(false)
 
   return (
     <>
       <div className="pointer-events-none fixed flex h-screen w-full items-end justify-end p-6">
-        <Link
+        <button
+          onClick={() => setOpen(true)}
           className="btn btn-green-gradient pointer-events-auto gap-3 rounded-full px-8 py-4 shadow-xl"
-          href={`?modal=${UrlModal.ADD_WIDGET}`}
-          shallow
         >
           {t('add widget')}
           <PlusIcon className="fill-ocean-900" />
-        </Link>
+        </button>
       </div>
-      <AddWidgetModal isOpen={isOpen} onClose={closeModal} />
+      <AddWidgetModal isOpen={isOpen} onClose={() => setOpen(false)} />
     </>
   )
 }
