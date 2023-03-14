8ef06c359eb0cb4a94c20cd54496370f1eae1f75
gregs <ukfaeku@gmail.com>
2023-03-14 18:17:38 -0300

    a

--- frontend/app/[asset]/index.tsx
+++ frontend/app/[asset]/index.tsx
@@ -13,7 +13,6 @@ export default function Home({ dehydratedState }: { dehydratedState: SuperJSONRe
   return (
     <Hydrate state={state}>
       <WidgetsProvider>
-        {/* <Title /> */}
         <StrategyHeader />
         <GridLayout />
       </WidgetsProvider>
