fc62395721cbe8c05c65b3e762174a66bd940842
21Chani <dmendoncacosta03@gmail.com>
2023-03-22 15:38:20 -0300

    unsopported network modal

--- frontend/app/providers/index.tsx
+++ frontend/app/providers/index.tsx
@@ -1,11 +1,15 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Modal, ModalProps } from '@tradex/interface'
 import { queryClientAtom } from 'jotai-tanstack-query'
 import { useHydrateAtoms } from 'jotai/utils'
+import Image from 'next/image'
 // import { TranslationProvider } from '@tradex/languages'
 import { PropsWithChildren, useEffect, useLayoutEffect, useState } from 'react'
 import { Theme } from 'utils/themeHandler'
+import { useNetwork, useSwitchNetwork } from 'wagmi'
+import { optimism } from 'wagmi/chains'
 import { IsHydratedProvider } from './IsHydratedProvider'
 import { ThemeProvider } from './ThemeProvider'
 import { WagmiProvider } from './WagmiProvider'
@@ -22,15 +26,50 @@ export default function AppProviders({ children }: PropsWithChildren) {
     Theme.toDOM(theme)
   }, [])
 
+  const { chain } = useNetwork()
+
   return (
     <IsHydratedProvider>
       <WagmiProvider>
         {/* <TranslationProvider> */}
         <QueryClientProvider client={queryClient}>
+          <UnsupportedNetworkModal isOpen={chain?.id !== 10} onClose={() => {}} />
+
           <ThemeProvider>{children}</ThemeProvider>
         </QueryClientProvider>
         {/* </TranslationProvider> */}
       </WagmiProvider>
     </IsHydratedProvider>
+  )
+}
+
+function UnsupportedNetworkModal(props: ModalProps) {
+  const { switchNetwork } = useSwitchNetwork()
+  return (
+    <Modal {...props}>
+      <div className="` h-screen">
+        <div className="card card-primary-outlined gap- mt-20 h-fit w-[450px] p-6">
+          <span className="text-dark-accent ocean:text-blue-accent mx-auto text-xl ">
+            Unsupported network
+          </span>
+          <span className="text-dark-30 ocean:text-blue-30 mx-auto text-lg">
+            Please switch to optimism
+          </span>
+          <button
+            className="btn btn-secondary centered mt-4 h-14 shadow-lg "
+            onClick={() => switchNetwork?.(optimism.id)}
+          >
+            <Image
+              src={'/assets/networks/optimism.png'}
+              priority
+              width={30}
+              height={30}
+              alt="op logo"
+            />
+            Switch to optimism
+          </button>
+        </div>
+      </div>
+    </Modal>
   )
 }
