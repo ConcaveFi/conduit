32ba4a6c0f0ae34eacf3f0cdc2dcf8d0473f77df
21Chani <dmendoncacosta03@gmail.com>
2023-03-22 14:58:13 -0300

    add & remove widgets / match figma & more

--- frontend/app/providers/WidgetsProvider.tsx
+++ frontend/app/providers/WidgetsProvider.tsx
@@ -1,61 +1,9 @@
 'use client'
 
 import { createContext, MutableRefObject, useContext, useRef, useState } from 'react'
+import { WidgetEvents } from 'src/entities/widgets.events'
 import { GridWidget, GridWidgets } from 'utils/grid/grid.widgets'
 import { AddWidgetOverlay } from '../[asset]/components/widgets/AddWidgetOverlay'
-
-type AddWidgetsEventListener = { event: 'add'; callback(widgets: GridWidgets[]): void }
-type RemoveWidgetsEventListener = { event: 'remove'; callback(widget: GridWidgets): void }
-
-class WidgetEvents {
-  private addWidgetsListeners: AddWidgetsEventListener[] = []
-  private removeWidgetsListeners: RemoveWidgetsEventListener[] = []
-
-  public on(event: 'add', callback: (widgets: GridWidgets[]) => void): void
-  public on(event: 'remove', callback: (widgets: GridWidgets) => void): void
-  public on(event: 'add' | 'remove', callback) {
-    if (event === 'add') {
-      this.addWidgetsListeners.push({ event, callback })
-    } else {
-      this.removeWidgetsListeners.push({ event, callback })
-    }
-  }
-
-  public off(event: 'add', callback: (widgets: GridWidgets[]) => void): void
-  public off(event: 'remove', callback: (widgets: GridWidgets) => void): void
-  public off(event: 'add' | 'remove', callback): void {
-    if (event === 'add') {
-      const listenerIndex = this.addWidgetsListeners.findIndex((v, i) => {
-        return v.callback.toString() === callback.toString() && v.event === event
-      })
-      this.addWidgetsListeners.splice(listenerIndex, 1)
-    } else {
-      const listenerIndex = this.removeWidgetsListeners.findIndex((v, i) => {
-        return v.callback.toString() === callback.toString() && v.event === event
-      })
-      this.removeWidgetsListeners.splice(listenerIndex, 1)
-    }
-  }
-
-  public getAddEventListeners(): AddWidgetsEventListener[] {
-    return this.addWidgetsListeners
-  }
-
-  public getRemoveEventListeners(): RemoveWidgetsEventListener[] {
-    return this.removeWidgetsListeners
-  }
-
-  public callListeners(event: 'add', widgets: GridWidgets[]): void
-  public callListeners(event: 'remove', widget: GridWidgets): void
-  public callListeners(event: 'add' | 'remove', payload: GridWidgets | GridWidgets[]): void {
-    if (event === 'add' && Array.isArray(payload)) {
-      this.addWidgetsListeners.forEach((listener) => listener.callback(payload))
-    } else if (event === 'remove' && typeof payload === 'string') {
-      this.removeWidgetsListeners.forEach((listener) => listener.callback(payload))
-    }
-  }
-}
-
 interface WidgetsContext {
   addWidgets(_widgets: GridWidgets[]): void
   removeWidget(widget: GridWidgets): Promise<GridWidgets | undefined>
@@ -80,9 +28,9 @@ export function WidgetsProvider({ children }: any) {
 
   /**
    * This function will change the state with the added widgets
-   * and store on the localstore those added widgets
+   * and store on the localstorage those added widgets
    * after everything is done, events listeners are triggered
-   * @param w
+   * @param _widgets is the array of widgets wanted to be added
    */
   function addWidgets(_widgets: GridWidgets[]) {
     const newWidgets = [...widgets]
@@ -96,9 +44,9 @@ export function WidgetsProvider({ children }: any) {
 
   /**
    * This function will change the state with the removed widgets
-   * and store on the localstore those removed widgets
+   * and store on the localstorage those removed widgets
    * after everything is done, events listeners are triggered.
-   * @param w
+   * @param widget is the object wanted to be removed
    */
   async function removeWidget(widget: GridWidgets) {
     const _widgets = widgets.filter((w) => w !== widget)
