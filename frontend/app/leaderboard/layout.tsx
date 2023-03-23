6883df295ccc40dbae57a293e696b020f90182cd
21Chani <dmendoncacosta03@gmail.com>
2023-03-23 15:49:34 -0300

    sidebar adjustmens

--- frontend/app/leaderboard/layout.tsx
+++ frontend/app/leaderboard/layout.tsx
@@ -1,8 +1,10 @@
+import { Sidebar } from 'app/[asset]/components/sidebar/Sidebar'
 import { Topbar } from 'app/[asset]/components/topbar/Topbar'
 
 export default async function Layout({ children }) {
   return (
     <div className=" h-full w-full p-4">
+      <Sidebar />
       <Topbar />
       {children}
     </div>
