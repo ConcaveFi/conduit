77d0b314638a291b8f0c3425485310028e80467e
21Chani <dmendoncacosta03@gmail.com>
2023-04-19 18:55:51 -0300

    remove uncomplete or unused

--- frontend/app/[asset]/components/ViewPanels.tsx
+++ frontend/app/[asset]/components/ViewPanels.tsx
@@ -4,12 +4,10 @@ import { LanguageKeys, useTranslation } from '@tradex/languages'
 import { forwardRef } from 'react'
 import { useRouteMarket } from '../lib/market/useMarket'
 import { UserPositions } from './positions/UserPositions'
-import { UserTrades } from './positions/UserTrades'
-import { UserTransfers } from './positions/UserTransfers'
 
 // CONSTANTS -------------------------------------------------------------------
 
-const TABS = ['trade', 'orders', 'trading_history', 'transfers'] as LanguageKeys[]
+const TABS = ['trade' /*, 'orders', 'trading_history', 'transfers'*/] as LanguageKeys[]
 
 // COMPONENT ---------------------------------------------------------------------
 
@@ -28,7 +26,7 @@ export const ViewPanels = forwardRef<HTMLDivElement, PanelProps>((props, ref) =>
           <Tab.Panel className={'h-full w-full '}>
             <UserPositions />
           </Tab.Panel>
-          <Tab.Panel>
+          {/* <Tab.Panel>
             <></>
           </Tab.Panel>
           <Tab.Panel>
@@ -36,7 +34,7 @@ export const ViewPanels = forwardRef<HTMLDivElement, PanelProps>((props, ref) =>
           </Tab.Panel>
           <Tab.Panel>
             <UserTransfers />
-          </Tab.Panel>
+          </Tab.Panel> */}
         </Tab.Panels>
         {props.children}
       </Panel>
